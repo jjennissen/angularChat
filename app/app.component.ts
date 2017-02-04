225683d00905b879d174cb8c84120c8e17a2c3ed
jjennissen <jjennissen@users.noreply.github.com>
2017-02-04 10:02:03 -0600

    Added Chat Service

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+import { ChatService } from './chat/chat.service';
+
 @Component({
     selector: 'main-app',
     template: `
         <chat></chat>
-    `
+    `,
+    providers: [ChatService]
 })
 export class AppComponent { }
